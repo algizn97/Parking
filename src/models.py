d685470b41698f92e3e63ee56d96fe74bf206e69
algizn97 <algiz_n@mail.ru>
2025-06-28 21:41:42 +0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,47 +1,62 @@
-from . import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import relationship
+
+db = SQLAlchemy()
 
 
-class Client(db.Model):
+class Client(db.Model):  # type: ignore[name-defined]
     """
     Модель клиента.
     """
-    __tablename__ = 'client'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50), nullable=False)
-    credit_card = db.Column(db.String(50))
-    car_number = db.Column(db.String(10))
+    __tablename__ = "client"
 
-    parking_logs = db.relationship('ClientParking', backref='client', lazy=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(50), nullable=False)
+    surname = Column(String(50), nullable=False)
+    credit_card = Column(String(50))
+    car_number = Column(String(10))
+
+    parking_logs = relationship("ClientParking", backref="client", lazy=True)
 
 
-class Parking(db.Model):
+class Parking(db.Model):  # type: ignore[name-defined]
     """
     Модель парковки.
     """
-    __tablename__ = 'parking'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    address = db.Column(db.String(100), nullable=False)
-    opened = db.Column(db.Boolean)
-    count_places = db.Column(db.Integer, nullable=False)
-    count_available_places = db.Column(db.Integer, nullable=False)
+    __tablename__ = "parking"
 
-    client_logs = db.relationship('ClientParking', backref='parking', lazy=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    address = Column(String(100), nullable=False)
+    opened = Column(Boolean)
+    count_places = Column(Integer, nullable=False)
+    count_available_places = Column(Integer, nullable=False)
+
+    client_logs = relationship("ClientParking", backref="parking", lazy=True)
 
 
-class ClientParking(db.Model):
+class ClientParking(db.Model):  # type: ignore[name-defined]
     """
     Модель связи клиента и парковки.
     """
-    __tablename__ = 'client_parking'
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
-    parking_id = db.Column(db.Integer, db.ForeignKey('parking.id'), nullable=False)
-    time_in = db.Column(db.DateTime, nullable=False)
-    time_out = db.Column(db.DateTime, nullable=True)
+    __tablename__ = "client_parking"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    client_id = Column(Integer, ForeignKey("client.id"), nullable=False)
+    parking_id = Column(Integer, ForeignKey("parking.id"), nullable=False)
+    time_in = Column(DateTime, nullable=False)
+    time_out = Column(DateTime, nullable=True)
     __table_args__ = (
-        db.UniqueConstraint('client_id', 'parking_id', name='unique_client_parking'),
+        UniqueConstraint("client_id", "parking_id", name="unique_client_parking"),
     )
